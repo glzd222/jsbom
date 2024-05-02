557102aca1367b8eb56146fa7135f0383cd30c3a
glzd222 <mi85h7wkd@code.edu.az>
2024-05-02 08:57:33 +0400

    change

---
+++ script.js
@@ -0,0 +1,13 @@
+const alarm =document.querySelector(".alarm");
+const texthour =document.querySelector(".texthour");
+const textminute =document.querySelector(".textminute")
+const textsecond =document.querySelector(".textsecond")
+const span =document.querySelector(".span")
+const hourspan =document.querySelector(".hourspan")
+const minutespan =document.querySelector(".minutespan")
+const secondspan =document.querySelector(".secondspan")
+const p =document.querySelector(".p") 
+const phour =document.querySelector(".phour")
+const pminute =document.querySelector(".pmiute")
+const psecond =document.querySelector(".psecond")
+const button =document.querySelector(".button")
